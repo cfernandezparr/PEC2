1336050a9211fe4bbae3be17eba005960ea990bd
[Carlos Fernández Parras] <[cfernandezparr@uoc.edu]>
2023-11-03 17:04:39 +0100

    Ejercicio 1a

---
+++ PEC2_Ej1/ejer1-a.js
@@ -0,0 +1,42 @@
+
+// Se declara la función findOne que tiene tres argumentos: un array, un objeto con la propiedad key
+//  y el valor value y otro objeto que tiene dos funciones callback que se llamaran una u otra si se
+//  cumple la condición o no de busqueda.
+const findOne = (list, { key, value }, { onSuccess, onError }) => {
+
+
+
+    setTimeout(() => {  // La funcion setTimeOut establece un retraso de 2 segundos antes de ejecutar la función.
+      const element = list.find(element => element[key] === value); //Se hace busqueda con el metodo find() del array list si coincide la key de element estrictanente al valor value.
+      element ? onSuccess(element) : onError({ msg: 'ERROR: Element Not Found' }); // Si coincide la busqueda se ejecuta onSuccess y sino onError
+    }, 2000); //Se establece el retraso de 2 segundos.
+  };
+  
+  const onSuccess = ({ name }) => console.log(`user: ${name}`); //Se declara la función onSuccess que muestra por consola el nombre del usuario encontrado.
+  const onError = ({ msg }) => console.log(msg); // Se declara la función onError que muestra un mensaje de error por consola.
+  
+  const users = [  // Se declara el array con los objetos user.
+    {
+      name: 'Carlos', //Clave
+      rol: 'Teacher' //Valor
+    },
+    {
+      name: 'Ana', //Clave
+      rol: 'Boss' //Valor
+    }
+  ];
+  
+  console.log('findOne success'); //se muestra mensaje por consola.
+  findOne(users, { key: 'name', value: 'Carlos' }, { onSuccess, onError }); //Se ejecuta la función findOne con el value Carlos que dará un resultado satisfactorio.
+  
+  console.log('findOne error'); //se muestra mensaje por consola.
+  findOne(users, { key: 'name', value: 'Fermin' }, { onSuccess, onError }); //Se ejecuta la función findOne con el value Fermin que dará un resultado de error.
+  
+  /*
+  findOne success
+  findOne error
+   //wait 2 seconds
+  user: Carlos
+  ERROR: Element Not Found
+  */
+  
---
+++ README.md
@@ -0,0 +1,3 @@
+• cfernandezparr
+• Carlos Fernández Parras.
+• Breve descripción de lo realizado en esta PEC, dificultades, mejoras realizadas, si hay que tener algo en cuenta a la hora de corregir/ejecutar la practica o cualquier aspecto que queráis destacar.
